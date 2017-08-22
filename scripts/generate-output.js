c08302b82472b22da05ded321a36c98fc714e5bb
Sam Lau <esayemm@gmail.com>
2017-08-22 00:38:36 -0700

    variable into js

--- scripts/generate-output.js
+++ scripts/generate-output.js
@@ -1,17 +1,32 @@
 const fs = require('fs')
 const path = require('path')
-const variables = require('../variables.js')
 
-const sassFilePath = path.resolve(__dirname, '../variables.scss')
+const SASS_FILEPATH = path.resolve(__dirname, '../variables.scss')
 
-const generatedContent = Object.keys(variables).map(key => {
-  return `$${key}: ${variables[key]};`
-}).join('\n')
+function extractValues(obj) {
+  let generatedContent = ''
+  for (let key in obj) {
+    if (typeof obj[key] === 'string') {
+      generatedContent += `$${key}: ${obj[key]};\n`
+    } else if (typeof obj[key] === 'object') {
+      generatedContent += extractValues(obj[key])
+    }
+  }
+  return generatedContent
+}
+function generateContent() {
+  const variables = require('../variables.js')
+  return extractValues(variables)
+}
 
-const sassFileContent = fs.readFileSync(sassFilePath, { encoding: 'utf8' }).split('\n')
-const startLine = sassFileContent.findIndex(line => line === '// start generate-output')
-const endLine = sassFileContent.findIndex(line => line === '// end generate-output')
-sassFileContent.splice(startLine+1, endLine - startLine - 1, generatedContent)
-const sassFileContentString = sassFileContent.join('\n')
+function writeToSassFile(generatedContent) {
+  const sassFileContent = fs.readFileSync(SASS_FILEPATH, { encoding: 'utf8' }).split('\n')
+  const startLine = sassFileContent.findIndex(line => line === '// start generate-output')
+  const endLine = sassFileContent.findIndex(line => line === '// end generate-output')
+  sassFileContent.splice(startLine+1, endLine - startLine - 1, generatedContent)
+  const sassFileContentString = sassFileContent.join('\n')
 
-fs.writeFileSync(sassFilePath, sassFileContentString)
+  fs.writeFileSync(SASS_FILEPATH, sassFileContentString)
+}
+
+writeToSassFile(generateContent())
