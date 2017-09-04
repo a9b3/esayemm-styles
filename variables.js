bf392e6cbeb765e8e6fe9ffe56830ae4e70e99c1
Sam Lau <esayemm@gmail.com>
2017-09-04 00:51:08 -0700

    remove unused

--- variables.js
+++ variables.js
@@ -7,63 +7,16 @@ module.exports = {
    */
 
   colors: {
-    gray0: '#29292A',
-    gray1: '#3E3E3E',
-    gray2: '#666667',
-    gray3: '#A3A3A5',
-    gray4: '#CCCCCE',
-    gray5: '#E0E0E2',
-    gray: '#363732',
-
-    cyan0: '#3B605B',
-    cyan1: '#53867F',
-    cyan2: '#6AACA3',
-    cyan3: '#81D2C7',
-    cyan4: '#97DAD1',
-    cyan5: '#BAE6E0',
-    cyan: '#81D2C7',
-
-    blue0: '#24394B',
-    blue1: '#365570',
-    blue2: '#416788',
-    blue3: '#7490A8',
-    blue: '#66C3FF',
-
-    yellow0: '#A37908',
-    yellow1: '#D19C0A',
-    yellow2: '#FFBE0B',
-    yellow3: '#FFC937',
-    yellow4: '#FFD563',
-    yellow5: '#FFE190',
-    yellow: '#FFC937',
-
-    pink0: '#73493F',
-    pink1: '#A06658',
-    pink2: '#CE8371',
-    pink3: '#FB9F89',
-    pink4: '#FBB09E',
-    pink5: '#FCC1B3',
-    pink: '#D4AFB9',
-
-    green0: '#0F4A37',
-    green1: '#15674E',
-    green2: '#1C8464',
-    green3: '#21A179',
-    green4: '#49B291',
-    green5: '#85CBB5',
-    green: '#21A179',
-
-    red0: '#6A3C3E',
-    red1: '#8D5152',
-    red2: '#B06466',
-    red3: '#C67B7D',
-    red4: '#D7A2A4',
-    red: '#C67B7D',
-
-    lightgray: '#DCE1E9',
-    blue1: '#53D8FB',
-    slate: '#2e3d49',
     white: '#ffffff',
+    gray: '#363732',
+    lightgray: '#DCE1E9',
+    cyan: '#81D2C7',
+    blue: '#66C3FF',
+    yellow: '#FFC937',
+    pink: '#D4AFB9',
+    green: '#21A179',
+    red: '#C67B7D',
+    slate: '#2e3d49',
 
     brand: '#D4AFB9',
     secondary: '#0D0A08',
@@ -73,6 +26,7 @@ module.exports = {
 
     defaultText: '#666667',
     defaultTextSecondary: tinycolor('#666667').setAlpha(.8).toString(),
+    headerColor: tinycolor('#666667').darken(50).toString(),
   },
 
   /*
@@ -80,6 +34,7 @@ module.exports = {
    */
 
   sizing: {
+    // Use for layout spacing (margin, padding etc).
     spacing1: '6rem',
     spacing2: '4rem',
     spacing3: '2rem',
@@ -87,12 +42,13 @@ module.exports = {
     spacing5: '.6rem',
     spacing6: '.4rem',
 
-    xsFont: '.5em',
-    smFont: '.85em',
-    mdFont: '1em',
-    lgFont: '1.28em',
-    xlFont: '1.71em',
-    xxlFont: '2.85em',
+    // Use for font sizes.
+    size1: '3em',
+    size2: '2.25em',
+    size3: '1.75em',
+    size4: '1.125em',
+    size5: '1em',
+    size6: '.8em',
 
     maxWidth: '80rem',
   },
