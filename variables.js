7df864c82854d76850c81d12c6e1eb76e8ce7565
Sam Lau <esayemm@gmail.com>
2017-09-05 01:19:08 -0700

    update variables

--- variables.js
+++ variables.js
@@ -2,12 +2,10 @@ const tinycolor = require('tinycolor2')
 
 module.exports = {
 
-  /*
-   * COLORS
-   */
-
   colors: {
+    // Default set of colors, should probably avoid using these.
     white: '#ffffff',
+    black: '#000000',
     gray: '#363732',
     lightgray: '#DCE1E9',
     cyan: '#81D2C7',
@@ -18,20 +16,18 @@ module.exports = {
     red: '#C67B7D',
     slate: '#2e3d49',
 
+    // These have more semantic meaning, should prefer these over the generic
+    // color names.
     brand: '#D4AFB9',
     secondary: '#0D0A08',
     error: '#C67B7D',
     warning: '#FFC937',
     success: '#21A179',
 
-    defaultText: '#666667',
-    defaultTextSecondary: tinycolor('#666667').setAlpha(.8).toString(),
+    defaultColor: '#666667',
+    defaultColorSecondary: tinycolor('#666667').setAlpha(.8).toString(),
     headerColor: tinycolor('#666667').darken(50).toString(),
   },
-
-  /*
-   * SIZING
-   */
 
   sizing: {
     // Use for layout spacing (margin, padding etc).
@@ -43,11 +39,17 @@ module.exports = {
     spacing6: '.4rem',
 
     // Use for font sizes.
+    // h1
     size1: '3em',
+    // h2
     size2: '2.25em',
+    // h3
     size3: '1.75em',
+    // h4
     size4: '1.125em',
+    // default
     size5: '1em',
+    // small
     size6: '.8em',
 
     maxWidth: '80rem',
